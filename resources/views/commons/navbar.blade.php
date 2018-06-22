bc6e14f7478c285b165faaa9656d91eccf5e7d5a
Your Name <you@example.com>
2018-06-22 06:00:02 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,20 @@
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
+                @if (Auth::check())
+                <li><a href="#">Users</a></li>
+                <li class="dropdown">
+                    <a href="#"　class="dropdown-toggle" date-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name}},span class="caret"></span></a>
+                <ul class="dropdown-manu">
+                    <li><a href="#">My profile</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li>{!! link_to_route('login.get','Logout') !!}</li>
+                </ul>
+                </li>
+                @else
                 <li>{!! link_to_route('signup.get','Signup') !!}</li>
                 <li><a href="#">Login</a></li>
+                @endif
             </ul>
         </div>
         </div>
